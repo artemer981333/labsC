03cedb305939e906bbca6e565d3feee37a3d27d6
ArtemPC <artemer981333@gmail.com>
2019-10-18 21:07:00 +0700

    lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -13,18 +13,18 @@ void deleteSymbols(char** text, char symbol);
 
 char** input(const char* fileName)
 {
-	FILE* f = fopen("fileName", "r");
+	FILE* f = fopen(fileName, "r");
 	if (f == NULL)
 		return NULL;
 	char** text = malloc(sizeof(char*));
 	text[0] = NULL;
 	int i = 0;
-	while(feof(f) != EOF)
+	while(!feof(f))
 	{
 		text = realloc(text, sizeof(char*) * (i + 2));
 		text[i] = malloc(sizeof(char) * STR_LEN);
+		text[i+1] = NULL;
 		fgets(text[i], STR_LEN, f);
-		printf("%s\n", text[i]);
 		i++;
 	}
 	fclose(f);
@@ -33,15 +33,13 @@ char** input(const char* fileName)
 
 void output(const char* fileName, char** text)
 {
-	printf("Ввод\n");
-	FILE* f = fopen(fileName, "w");
+	FILE* f;
+	f = fopen(fileName, "w");
 	if (f == NULL)
 		return;
 	while(*text != NULL)
 	{
-		printf("Вывод\n");
 		fputs(*text, f);
-		puts(*text);
 		text++;
 	}
 	fclose(f);	
@@ -49,26 +47,21 @@ void output(const char* fileName, char** text)
 
 _Bool checkString(char* string, char symbol)
 {
-	char* p;
-	p = string;
-	while(*p != '\0')
+	while(*string != '\0')
 	{
-		if (*p == symbol)
+		if (*string == symbol)
 			return 0;
+		string++;
 	}
 	return 1;
 }
 
 void deleteSymbols(char** text, char symbol)
 {
-	printf("Начато удаление\n");
-	printf("%s\n", *text);
 	while(*text != NULL)
 	{
-		printf("Проверка\n");		
 		if (!checkString(*text, symbol))
 		{
-			printf("Удаление строки\n");
 			free(*text);
 			char** pp = text;
 			do
@@ -78,33 +71,37 @@ void deleteSymbols(char** text, char symbol)
 			}
 			while(*pp != NULL);
 		}
-		text++;
+		else
+			text++;
 	}
 }
 
 int main(int argc, char** argv)
 {
 	char** text;
+	char* newName;
 	setlocale(LC_ALL, "ru");
 
-	printf("Ввод %s\n", argv[1]);
-	text = input(argv[1]);
+	text = input(argv[1] + 1);
 
-	printf("Вывод\n");
-	output("new.txt", text);
+	deleteSymbols(text, argv[2][1]);
 
-	printf("Удаление\n");
-	deleteSymbols(text, argv[2][0]);
-
-	printf("Новое имя\n");
-	char* newName;
+	newName = malloc(sizeof(char) * STR_LEN);
 	strcpy(newName, "");
-	if (strchr(argv[1], '.') != NULL)
-		strncat(newName, argv[1], strchr(argv[0], '.') - argv[1]);
+	if (strchr(argv[1] + 1, '.') != NULL)
+		strncat(newName, argv[1] + 1, strchr(argv[1] + 1, '.') - argv[1] - 1);
 	else
-		strcat(newName, argv[1]);
+		strcat(newName, argv[1] + 1);
 	strcat(newName, ".new");
 	
-	printf("Вывод\n");
 	output(newName, text);
+
+	int i = 0;
+	while(text[i] != NULL)
+	{
+		free(text[i]);
+		i++;
+	}
+	free(text);
+	free(newName);
 }
